ac6bd0b4ab4b7fbc66049fac28f7cee3999de128
idamiro <148451250+idamiro@users.noreply.github.com>
2026-06-26 17:08:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const statusText = document.querySelector(".form-status");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  statusText.textContent = "Sorğu qeydə alındı. Real saytda bu mesaj WhatsApp və ya e-poçta yönləndiriləcək.";
+  statusText.textContent = "Sor\u011fu qey\u0259 al\u0131nd\u0131. Real saytda bu mesaj WhatsApp v\u0259 ya e-po\u00e7ta y\u00f6nl\u0259ndiril\u0259c\u0259k.";
   form.reset();
 });
